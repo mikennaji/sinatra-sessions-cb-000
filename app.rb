3d204981f73e49047d3ead2f141f1edbad27fd72
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-24 13:39:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
-    session[:id] = params[:id]
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
